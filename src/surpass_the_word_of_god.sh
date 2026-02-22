2fc3128c9a8e23bdc12b73cc2e6d607e894d1723
jbreija <jmbreija@gmail.com>
2026-02-22 18:47:21 -0500

    Surpass the word of god

---
+++ src/surpass_the_word_of_god.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# All the world's laws and religions are in the wrong format. Only a fool would claim a wall of text is divine or intellect. We need to leverage graphs, tables, charts, visualizations etc... where appropriate.
+# "The word of god" doesn't make sense. It's like saying the bicycle of god. Do you know how I know that's not the bicycle of god? God doesn't ride bikes. Do you know how I know the bible is not the word of god?
+for every_time_someone_claims_a_wall_of_text_is_divine do
+	cat current_word_of_god.txt map_of_earth.png new_and_improved_divine_communication_that_leverages_a_map.png
+
+#TODO: make cat command work with txt + png ... but that's not really the point.
